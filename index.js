dafc1a896800607d118861f4809cac373660d5cf
Phil <phil.github@gmail.com>
2024-09-16 15:24:34 +0300

    incomplete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+  return array.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (list) {
+  const lastElement = list[list.length - 1];
+  const secondLast = list[list.length - 2];
+  return lastElement && secondLast;
+};
